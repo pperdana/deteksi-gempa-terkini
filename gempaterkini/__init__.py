d438442357259de81e34a770ea04710b4b2780f0
Panji <panji9502@gmail.com>
2021-11-10 14:43:07 +0700

    Menambahkan basic packet request dan beautifulsoup

--- gempaterkini/__init__.py
+++ gempaterkini/__init__.py
@@ -1,14 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+
+
 def ekstraksi_data():
-    hasil = dict()
-    hasil['tanggal'] = "24 Agustus 2021"
-    hasil['waktu'] = "12:05:52 WIB"
-    hasil['magnitudo'] = 4.0
-    hasil['lokasi'] = {'LS': 1.48, 'bt': 134.01}
+    try:
+        content = requests.get("https://www.b mkg.go.id/")
+    except Exception:
+        return None
 
-    return hasil
+    print(content.status_code)
+    # soup = BeautifulSoup(content)
+    if content.status_code == 200:
+        hasil = dict()
+        hasil['tanggal'] = "24 Agustus 2021"
+        hasil['waktu'] = "12:05:52 WIB"
+        hasil['magnitudo'] = 4.0
+        hasil['lokasi'] = {'LS': 1.48, 'bt': 134.01}
 
+        return hasil
+    else:
+        return None
 
 def tampilkan_data(result):
+    if result is None:
+        print("Tidak menemukan data gempa terkini")
+        return
     print("Gempa terakhir berdasakan BMKG")
     print(f"Tanggal {result['tanggal']}")
     print(f"Waktu {result['waktu']}")
