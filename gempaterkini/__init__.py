28283e2980b81530e0501421bc25952ec64863d5
Panji <panji9502@gmail.com>
2021-11-10 13:25:10 +0700

    Modularisai dengan function dengan penambahan packet

---
+++ gempaterkini/__init__.py
@@ -0,0 +1,17 @@
+def ekstraksi_data():
+    hasil = dict()
+    hasil['tanggal'] = "24 Agustus 2021"
+    hasil['waktu'] = "12:05:52 WIB"
+    hasil['magnitudo'] = 4.0
+    hasil['lokasi'] = {'LS': 1.48, 'bt': 134.01}
+
+    return hasil
+
+
+def tampilkan_data(result):
+    print("Gempa terakhir berdasakan BMKG")
+    print(f"Tanggal {result['tanggal']}")
+    print(f"Waktu {result['waktu']}")
+    print(f"Magnitudo {result['magnitudo']}")
+    print(f"Lokasi {result['lokasi']}")
+
